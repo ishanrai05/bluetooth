b02719e8860a31461eaa0698b1a144e50cdb1f3c
Ishan <ishanrai05@gmail.com>
2018-10-05 05:24:16 +0530

    updated rfcomm client-server code

--- rfcomm-client.py
+++ rfcomm-client.py
@@ -1,6 +1,17 @@
 import bluetooth
 
-bd_addr = "01:23:45:67:89:AB"
+target_name = "My Phone"
+target_address = None
+flag=0
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+    print(" %s - %s" % (addr, name))
+
+#target_name = input("Enter the target name of the device")
+target_address = input("Enter the target address of the device")
+bd_addr = target_address
 
 port = 1
 
